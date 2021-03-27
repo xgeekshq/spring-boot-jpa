00466892405399c13177eb7113bfdca983d44bf2
Otavio Santana <otaviopolianasantana@gmail.com>
2021-03-27 20:25:46 +0000

    Updates id strategy in the car

--- src/main/java/io/xgeeks/examples/spring/Car.java
+++ src/main/java/io/xgeeks/examples/spring/Car.java
@@ -13,7 +13,7 @@ import java.util.Objects;
 public class Car {
 
     @Id
-    @GeneratedValue(strategy= GenerationType.AUTO)
+    @GeneratedValue(strategy= GenerationType.IDENTITY)
     private Long id;
     @Column
     private String name;
